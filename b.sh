91d4f49e11468793c0b06ab0844c4c948a8af0d2
mhtran <hoangtm@runsystem.net>
2017-11-13 16:05:47 +0700

    Fix name same cocoa pod

--- b.sh
+++ b.sh
@@ -1,14 +1,14 @@
 pod init open -a Xcode Podfile
-pod spec create SWUtils
+pod spec create SWUtil
 git init
 git add . 
-# git commit -m "Initial commit" 
-# git tag 0.1.0 
-# git remote add origin https://github.com/mhtranbn/SWUtils.git
-# git push -u origin master --tags
-# pod repo add SWUtils https://github.com/mhtranbn/SWUtils.git
-# pod repo push SWUtils SWUtils.podspec --allow-warnings
-# pod lib lint --allow-warnings
-# pod repo update
-# pod trunk register hoangtm@runsystem.net 'hoangtm' --description='macbook pro'
-# pod trunk push SWUtils.podspec --allow-warnings --verbose
+git commit -m "Initial commit" 
+git tag 0.1.0 
+git remote add origin https://github.com/mhtranbn/SWUtils.git
+git push -u origin master --tags
+pod repo add RSUtils https://github.com/mhtranbn/SWUtils.git
+pod repo push SWUtil SWUtil.podspec --allow-warnings
+pod lib lint --allow-warnings
+pod repo update
+pod trunk register hoangtm@runsystem.net 'hoangtm' --description='macbook pro'
+pod trunk push SWUtil.podspec --allow-warnings --verbose
