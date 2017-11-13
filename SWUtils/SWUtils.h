8ede7e468ca11666c6c2eb0d2d0215382bc46c29
mhtran <hoangtm@runsystem.net>
2017-11-13 14:17:12 +0700

    Initial commit

---
+++ SWUtils/SWUtils.h
@@ -0,0 +1,19 @@
+//
+//  SWUtils.h
+//  SWUtils
+//
+//  Created by mhtran on 11/13/17.
+//  Copyright © 2017 mhtran. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SWUtils.
+FOUNDATION_EXPORT double SWUtilsVersionNumber;
+
+//! Project version string for SWUtils.
+FOUNDATION_EXPORT const unsigned char SWUtilsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SWUtils/PublicHeader.h>
+
+
---
+++ b.sh
@@ -0,0 +1,14 @@
+pod init open -a Xcode Podfile
+pod spec create SWUtils
+git init
+git add . 
+git commit -m "Initial commit" 
+# git tag 0.1.0 
+# git remote add origin https://github.com/mhtranbn/SWUtils.git
+# git push -u origin master --tags
+# pod repo add SWUtils https://github.com/mhtranbn/SWUtils.git
+# pod repo push SWUtils SWUtils.podspec --allow-warnings
+# pod lib lint --allow-warnings
+# pod repo update
+# pod trunk register hoangtm@runsystem.net 'hoangtm' --description='macbook pro'
+# pod trunk push SWUtils.podspec --allow-warnings --verbose
